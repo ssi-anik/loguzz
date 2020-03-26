a98d1c1d9405f5b6e0ad5b048aca228a6d71d9c0
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2020-03-26 16:58:28 +0600

    test case for response added

---
+++ tests/Formatter/Response/ResponseArrayFormatterTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use Loguz\Formatter\ResponseArrayFormatter;
+use PHPUnit\Framework\TestCase;
+
+class ResponseArrayFormatterTest extends TestCase
+{
+    /**
+     * @var AbstractResponseFormatter
+     */
+    protected $formatter;
+    /** @var Client */
+    protected $client;
+
+    public function setUp () : void {
+        $this->client = new Client([
+            'base_uri'   => 'https://httpbin.org',
+            'user-agent' => 'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)',
+        ]);
+        
+        $this->formatter = new ResponseArrayFormatter();
+    }
+
+    public function testResponseKey () {
+        $request = new Request('GET', '/get');
+        $response = $this->client->send($request);
+        $format = $this->formatter->format($response);
+
+        $this->assertArrayHasKey("protocol", $format);
+        $this->assertArrayHasKey("reason_phrase", $format);
+        $this->assertArrayHasKey("status_code", $format);
+        $this->assertArrayHasKey("headers", $format);
+        $this->assertArrayHasKey("size", $format);
+        $this->assertArrayHasKey("body", $format);
+    }
+}
