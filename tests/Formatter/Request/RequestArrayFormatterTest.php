7dcdef16df2ef60d153c042b8d75886d7d0afb25
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-10-08 09:02:31 +0600

    request array formatter uses base request formatter test class

--- tests/Formatter/Request/RequestArrayFormatterTest.php
+++ tests/Formatter/Request/RequestArrayFormatterTest.php
@@ -1,10 +1,9 @@
 <?php
 
-use GuzzleHttp\Cookie\CookieJar;
 use Loguzz\Formatter\RequestArrayFormatter;
-use Loguzz\Test\FormatterTestCase;
+use Loguzz\Test\Formatter\Request\RequestFormatterTest;
 
-class RequestArrayFormatterTest extends FormatterTestCase
+class RequestArrayFormatterTest extends RequestFormatterTest
 {
     /**
      * @var RequestArrayFormatter
@@ -16,141 +15,100 @@ class RequestArrayFormatterTest extends FormatterTestCase
         return new RequestArrayFormatter();
     }
 
-    public function testUserAgent()
+    public function implementAssertionForUserAgent($response)
     {
-        $response = $this->formatter->format($this->createRequest());
-
         $this->assertArrayHasKey("user-agent", $response);
         $this->assertEquals(self::USER_AGENT, $response['user-agent']);
     }
 
-    public function testRequestMethodIsIncluded()
+    public function implementAssertionForRequestMethodIsIncluded($response)
     {
-        $response = $this->formatter->format($this->createRequest());
-
         $this->assertArrayHasKey("method", $response);
     }
 
-    public function testHeadersAreIncluded()
+    public function implementAssertionForHeadersAreIncluded($response)
     {
-        $response = $this->formatter->format($this->createRequest('GET', '/', '', ['foo' => 'bar']));
-
         $this->assertArrayHasKey("headers", $response);
         $this->assertArrayHasKey("foo", $response['headers']);
         $this->assertEquals(["bar"], $response['headers']['foo']);
     }
 
-    public function testSameHeadersWithMultipleValues()
+    public function implementAssertionForSameHeadersWithMultipleValues($response)
     {
-        $response = $this->formatter->format($this->createRequest('GET', '/', '', ['foo' => ['bar', 'baz']]));
-
         $this->assertEquals(["bar", "baz"], $response['headers']['foo']);
     }
 
-    public function testRequestContainsAllHeaders()
+    public function implementAssertionForRequestContainingAllHeaders($response)
     {
-        $headers = ['foo' => 'bar', 'baz' => 'baz',];
-        $response = $this->formatter->format($this->createRequest('get', '/', '', $headers));
-
         $this->assertArrayHasKey("headers", $response);
         $this->assertEquals(["bar"], $response['headers']['foo']);
         $this->assertEquals(["baz"], $response['headers']['baz']);
     }
 
-    public function testGetRequestWithQueryString()
+    public function implementAssertionForGetRequestWithQueryString($response)
     {
-        $response = $this->formatter->format($this->createRequest('get', 'http://example.local?foo=bar'));
-
         $this->assertArrayHasKey('url', $response);
         $this->stringContains('foo=bar', $response['url']);
     }
 
-    public function testGetRequestWithRequestBody()
+    public function implementAssertionForGetRequestWithRequestBody($response)
     {
-        $body = http_build_query(['foo' => 'bar', 'hello' => 'world']);
-        $response = $this->formatter->format($this->createRequest('get', '/', $body));
-
         $this->assertEquals('GET', $response['method']);
         $this->assertArrayHasKey('body', $response);
         $this->assertEquals('foo=bar&hello=world', $response['body']);
     }
 
-    public function testPostRequest()
+    public function implementAssertionForPostRequest($response)
     {
-        $body = http_build_query(['foo' => 'bar', 'hello' => 'world']);
-        $response = $this->formatter->format($this->createRequest('post', '/', $body));
-
         $this->assertEquals('POST', $response['method']);
         $this->assertNotEquals('GET', $response['method']);
         $this->assertArrayHasKey('body', $response);
         $this->assertEquals('foo=bar&hello=world', $response['body']);
     }
 
-    public function testHeadRequest()
+    public function implementAssertionForHeadRequest($response)
     {
-        $response = $this->formatter->format($this->createRequest('HEAD'));
-
         $this->assertEquals('HEAD', $response['method']);
     }
 
-    public function testOptionsRequest()
+    public function implementAssertionForOptionsRequest($response)
     {
-        $response = $this->formatter->format($this->createRequest('OPTIONS'));
-
         $this->assertEquals('OPTIONS', $response['method']);
     }
 
-    public function testDeleteRequest()
+    public function implementAssertionForDeleteRequest($response)
     {
-        $response = $this->formatter->format($this->createRequest('DELETE'));
-
         $this->assertEquals('DELETE', $response['method']);
     }
 
-    public function testPutRequest()
+    public function implementAssertionForPutRequest($response)
     {
-        $response = $this->formatter->format($this->createRequest('PUT', '/', 'foo=bar&hello=world'));
-
         $this->assertEquals('PUT', $response['method']);
         $this->assertArrayHasKey('body', $response);
         $this->assertEquals('foo=bar&hello=world', $response['body']);
     }
 
-    public function testProperBodyReading()
+    public function implementAssertionForPatchRequest($response)
     {
-        $request = $this->createRequest('PUT', '/', 'foo=bar&hello=world');
-        $content = $request->getBody()->getContents();
+        $this->assertEquals('PATCH', $response['method']);
+        $this->assertArrayHasKey('body', $response);
+        $this->assertEquals('foo=bar&hello=world', $response['body']);
+    }
 
-        $response = $this->formatter->format($request);
-
-        $this->assertEquals($content, $response['body']);
+    public function implementAssertionForProperBodyReading($response, $originalContent)
+    {
+        $this->assertEquals($originalContent, $response['body']);
         $this->assertEquals('foo=bar&hello=world', $response['body']);
         $this->assertEquals("PUT", $response['method']);
     }
 
-    public function testExtractBodyArgument()
+    public function implementAssertionForExtractBodyArgument($response)
     {
-        $headers = ['X-Foo' => 'Bar'];
-        $body = chr(0) . 'foo=bar&hello=world';
-        // clean input of null bytes
-        $body = str_replace(chr(0), '', $body);
-        $request = $this->createRequest('post', '/', $body, $headers);
-        $response = $this->formatter->format($request);
-
         $this->assertEquals('foo=bar&hello=world', $response['body']);
     }
 
-    public function testCookieIsParsedFromRequest()
+    public function implementAssertionForCookieIsParsedFromRequest($response)
     {
-        $request = $this->createRequest('GET', '/', 'foo=bar&hello=world');
-
-        $response = $this->formatter->format(
-            $request,
-            [
-                'cookies' => CookieJar::fromArray(['cookie-name' => 'cookie-value'], self::BASE_URI),
-            ]
-        );
-
         $this->assertArrayHasKey('cookies', $response);
         $this->assertCount(1, $response['cookies']);
         $this->assertEquals('cookie-name', $response['cookies'][0]['name']);
