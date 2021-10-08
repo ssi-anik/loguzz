8f6ba990699f6257f5c97374ed805cf1f2bd44ef
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-10-08 08:21:13 +0600

    exception formatter uses FormatterTestCase class

--- tests/Formatter/Exception/ResponseExceptionFormatterTest.php
+++ tests/Formatter/Exception/ResponseExceptionFormatterTest.php
@@ -1,34 +1,26 @@
 <?php
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Request;
-use Loguzz\Formatter\ExceptionArrayFormatter;
-use PHPUnit\Framework\TestCase;
+namespace Loguzz\Test\Formatter\Exception;
 
-class ResponseExceptionFormatterTest extends TestCase
+use Exception;
+use GuzzleHttp\Exception\ConnectException;
+use Loguzz\Formatter\ExceptionArrayFormatter;
+use Loguzz\Test\FormatterTestCase;
+
+class ResponseExceptionFormatterTest extends FormatterTestCase
 {
     /**
-     * @var AbstractResponseFormatter
+     * @var \Loguzz\Formatter\AbstractResponseFormatter
      */
     protected $formatter;
-    /** @var Client */
-    protected $client;
-
-    public function setUp(): void
-    {
-        $this->client = new Client([
-            'base_uri' => 'https://not.a.valid.url.here',
-            'user-agent' => 'guzzle-log-middleware',
-        ]);
-
-        $this->formatter = new ExceptionArrayFormatter();
-    }
 
     public function testException()
     {
-        $request = new Request('GET', '/get');
+        $request = $this->createRequest();
+        $response = new ConnectException('Error connecting the server.', $request);
+        $client = $this->getClient($response);
         try {
-            $this->client->send($request);
+            $client->send($request);
         } catch (Exception $e) {
             $format = $this->formatter->format($request, $e);
         }
@@ -37,5 +29,10 @@ class ResponseExceptionFormatterTest extends TestCase
         $this->assertArrayHasKey("class", $format);
         $this->assertArrayHasKey("code", $format);
         $this->assertArrayHasKey("message", $format);
+    }
+
+    protected function getFormatter(): ExceptionArrayFormatter
+    {
+        return new ExceptionArrayFormatter();
     }
 }
---
+++ tests/LoguzzTestCase.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Loguzz\Test;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
+use Loguzz\Middleware\LogMiddleware;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\Test\TestLogger;
+
+class LoguzzTestCase extends TestCase
+{
+    public const USER_AGENT = 'anik/loguzz guzzle-log-middleware';
+    protected $logger;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->logger = $this->getLogger();
+    }
+
+    protected function getClient($responses = [], array $options = [], ?LogMiddleware $logMiddleware = null): Client
+    {
+        $responses = is_array($responses) ? $responses : [$responses];
+        if ($responses) {
+            $handler = new MockHandler($responses);
+        } else {
+            $handler = HandlerStack::create();
+        }
+
+        if ($logMiddleware) {
+            $handler->push($logMiddleware, 'logger');
+        }
+
+        $options = $options + ['base_uri' => 'https://example.local', 'handler' => $handler,];
+
+        return new Client($options);
+    }
+
+    protected function getLogger(): TestLogger
+    {
+        return new TestLogger();
+    }
+
+    protected function getLoggerMiddleware(array $options = []): LogMiddleware
+    {
+        if (!$this->logger) {
+            throw new Exception('logger variable must be initialized before calling getLoggerMiddleware().');
+        }
+
+        return new LogMiddleware($this->logger, $options);
+    }
+
+    protected function createRequest(
+        $method = 'GET',
+        $url = '/',
+        $body = '',
+        array $headers = []
+    ): Request {
+        $headers = $headers + ['user-agent' => self::USER_AGENT,];
+
+        return new Request($method, $url, $headers, $body);
+    }
+}
